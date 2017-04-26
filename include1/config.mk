358109f4141178baafab61c05415cccb3202ef71
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2017-04-26 18:22:50 +0200

    On-the-fly generation of doconce file for include1

---
+++ include1/config.mk
@@ -0,0 +1,2 @@
+# which doconce files to include as dependencies
+DO_FILES   := ../do_files/code1.do.txt ../do_files/explain1.do.txt ../do_files/math1.do.txt
---
+++ include1/include1.prepare.sh
@@ -0,0 +1,13 @@
+echo "# Building up the output by adding successive doconce files"
+echo
+echo "===== Plain text ====="
+echo
+echo '# #include "../do_files/code1_nocode.do.txt"'
+echo
+echo "===== Explanation ====="
+echo
+echo '# #include "../do_files/explain1.do.txt"'
+echo
+echo "===== Math ====="
+echo
+echo '# #include "../do_files/math1.do.txt"'
