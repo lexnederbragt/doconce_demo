948ae6b01c6ab07c26fbf591b25762d21b96d983
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2017-04-26 14:24:43 +0200

    On-the-fly generation of doconce file for text1

---
+++ text1/config.mk
@@ -0,0 +1,2 @@
+# which elemtn doconce files to include as dependancies
+ELEMENTS   := ../elements/code1.do.txt ../elements/explain1.do.txt ../elements/math1.do.txt
---
+++ text1/text1.prepare.sh
@@ -0,0 +1,11 @@
+echo "===== Plain text ====="
+echo
+cat ../elements/code1.do.txt
+echo
+echo "===== Explanation ====="
+echo
+cat ../elements/explain1.do.txt
+echo
+echo "===== Math ====="
+echo
+cat ../elements/math1.do.txt
