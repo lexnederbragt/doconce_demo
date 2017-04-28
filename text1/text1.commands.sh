2bc305a09dac1cbb63b2dc3078fd495b220d8837
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2017-04-28 14:50:59 +0200

    WIP creation of commands.sh file for text1

---
+++ text1/text1.commands.sh
@@ -0,0 +1,9 @@
+# Building html
+doconce format html text1.do.txt
+# Building pdf
+doconce format pdflatex text1.do.txt --latex_code_style=vrb && \
+	pdflatex text1.tex
+# Building jupyter notebook
+doconce format ipynb text1.do.txt
+# Building markdown
+doconce format markdown text1.do.txt
