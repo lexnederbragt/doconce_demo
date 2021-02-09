3c537cb81e0933cd0419386544ee4545bddedd78
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2021-02-09 12:33:32 +0100

    Added a demo of references/citations; not yet working for pdf

---
+++ reference1/reference1.prepare.sh
@@ -0,0 +1,24 @@
+# Building up the doconce file by adding successive doconce files
+
+echo "===== Plain text ====="
+echo
+cat ../do_files/cite1.do.txt
+echo
+echo "===== Explanation ====="
+echo
+cat ../do_files/explain1.do.txt
+echo
+echo "===== Table ====="
+echo
+cat ../do_files/table1.do.txt
+echo
+echo "===== Figure ====="
+echo
+cat ../do_files/figure1.do.txt
+echo "===== Math ====="
+echo
+cat ../do_files/math1.do.txt
+echo
+echo "===== References ====="
+echo
+echo "BIBFILE: papers.pub"
