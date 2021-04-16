32b420f74f744cb280e013030aaf66ef886dffcb
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2021-04-16 14:04:42 +0200

    compile.sh -> back to commands.sh

--- scripts/format_commands.sh
+++ scripts/format_commands.sh
@@ -1,3 +1,3 @@
 # Adds an empty line after each command entry
-# in the compile.sh file to improve readability
+# in the commands.sh file to improve readability
 awk 'NR>1 && /^# Building/{print ""}{print $0}'
