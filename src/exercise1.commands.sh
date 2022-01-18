8618091bce3cbf31fdde68f90e426df5fbb6b334
Lex Nederbragt <lex.nederbragt@ibv.uio.no>
2022-01-18 14:25:33 +0100

    Enable stand-alone solutions notebook

--- src/exercise1.commands.sh
+++ src/exercise1.commands.sh
@@ -2,25 +2,29 @@
 
 # Building html
 # Target: ../pub/exercise1.html
-doconce format html exercise1.do.txt --execute=abort && \
-	mv exercise1.html ../pub/exercise1.html
+doconce format html exercise1.do.txt --execute=abort \
+	--output=../pub/exercise1.html && \
+	cd ../pub && ln -fs ../src/.doconce_figure_cache .
 
 # Building pdf
 # Target: ../pub/exercise1.pdf
-doconce format pdflatex exercise1.do.txt --latex_code_style=vrb --execute=abort && \
+doconce format pdflatex exercise1.do.txt --latex_code_style=vrb \
+	--execute=abort --toc_depth=3 && \
 	pdflatex exercise1.tex && \
 	mv exercise1.pdf ../pub/exercise1.pdf
 
-# Building jupyter notebook for exercises with answers and solutions
-# Target: ../pub/exercise1.ipynb
-doconce format ipynb exercise1.do.txt --execute=abort && \
-	mv exercise1.ipynb ../pub/exercise1-solutions.ipynb
-
 # Building jupyter notebook for exercises without answers and solutions
-doconce format ipynb exercise1.do.txt --execute=abort --without_solutions --without_answers && \
-	mv exercise1.ipynb ../pub/exercise1.ipynb
+# Target: ../pub/exercise1.ipynb
+doconce format ipynb exercise1.do.txt --execute=abort --without_solutions --without_answers \
+	\
+--output=../pub/exercise1.ipynb
 
 # Building markdown
 # Target: ../pub/exercise1.md
-doconce format markdown exercise1.do.txt && \
-	mv exercise1.md ../pub/exercise1.md
+doconce format markdown exercise1.do.txt \
+	--output=../pub/exercise1.md
+
+# Building jupyter notebook for exercises with answers and solutions
+# Target: ../pub/exercise1-solutions.ipynb
+doconce format ipynb exercise1.do.txt --execute=abort \
+	--output=../pub/exercise1-solutions.ipynb
